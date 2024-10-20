ccd96a7ee1208109c3c75c26925f46218ef751e0
Arif Can AYERDEM <arifcan_2000@hotmail.com>
2024-10-20 15:32:27 +0300

    Catch the insect
    Portfolio

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./app.scss" ;
+import Navbar from "./components/navbar/Navbar";
+import Test from "./Test";
+
+
+const App = () => {
+  return <div>
+
+    <section><Navbar/></section>
+    <section>Parallax</section>
+    <section>Services</section>
+    <section>parallax</section>
+    <section>Portfolio1</section>
+    <section>Portfolio2</section>
+    <section>Portfolio3</section>
+    <section>Contact</section>
+   
+   {/*
+    <Test/>
+    <Test/>*/}
+
+  </div>;
+};
+
+export default App;
